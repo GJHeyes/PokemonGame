59939a651afde35e3fdbd9566ba5fedb5fe77091
GJHeyes <gregory-heyes@hotmail.com>
2022-10-30 20:36:44 +0000

    fix bug regarding is flipped

--- js/transitions/isFlipped.js
+++ js/transitions/isFlipped.js
@@ -1,10 +1,12 @@
 document.addEventListener('click', (e) =>{ //e for event
     let cardClassList = e.target.parentNode.classList
-    if(cardClassList.length > 1 && cardcardClassList[1].includes('cpu')) {
-        if(flip === false && playerSelect.length !=0){
-            cardClassList.toggle('is-not-flipped')
-            cardClassList.toggle('is-flipped')
-            flip = true
+    if(cardClassList.length> 1){
+        if(cardcardClassList[1].includes('cpu')) {
+            if(flip === false && playerSelect.length !=0){
+                cardClassList.toggle('is-not-flipped')
+                cardClassList.toggle('is-flipped')
+                flip = true
+            }
         }
     }
 })
