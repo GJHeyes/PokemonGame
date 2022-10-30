9444885ab123f7fe47d9c2323e5ff9d9acf2ff51
GJHeyes <gregory-heyes@hotmail.com>
2022-10-30 19:51:43 +0000

    deleted all

--- js/battle/updateScore 3.js
+++
@@ -1,6 +0,0 @@
-function updateScore(){
-    playerChart.config.data.datasets[0].data[0] = playerScore 
-    computerChart.config.data.datasets[0].data[0] = computerScore 
-    playerChart.update()
-    computerChart.update()
-}
